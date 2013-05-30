2fdde1c8129199467a534bec243b730c03c4027c
billyoncetoldme <billyoncetoldme@gmail.com>
2013-05-30 18:13:55 -0400

    adding carousel

---
+++ js/main-ck.js
@@ -0,0 +1,1 @@
+$(function(){$("ul.nav a").bind("click",function(e){var t=$(this);$("html, body").stop().animate({scrollTop:$(t.attr("href")).offset().top},1500,"easeInOutExpo");e.preventDefault()})});
